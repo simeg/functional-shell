c84288b82b0e7f5a0c32ab81702a393b40544b86
Simon Egersand <s.egersand@gmail.com>
2020-01-27 20:28:20 +0100

    Clean up install script

--- install.sh
+++ install.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
 
+#
+# This script installs map and filter so that they are available on your $PATH
+#
+# It clones the repo, moves the files to /usr/local/bin and /usr/local/lib and
+# finally removes the cloned repo
+#
+
 set -e
 
-echo "Cloning repo..."
+readonly tmp_dir="/tmp/functional-shell"
 
-git clone https://github.com/simeg/functional-shell /tmp/functional-shell
 
-echo "Repo cloned!"
+echo "-- Cloning repo..."
 
-echo "Installing..."
+git clone https://github.com/simeg/functional-shell ${tmp_dir}
 
-cp -f /tmp/functional-shell/filter /usr/local/bin
-cp -f /tmp/functional-shell/map /usr/local/bin
+echo "-- Repo cloned!"
 
-cp -rf /tmp/functional-shell/fs /usr/local/lib
+echo "-- Installing..."
 
-echo "Done!"
+# Install map and filter
+cp -f ${tmp_dir}/map /usr/local/bin
+cp -f ${tmp_dir}/filter /usr/local/bin
+
+# Install operations
+cp -rf ${tmp_dir}/fs /usr/local/lib
+
+echo "-- Cleaning up temp files..."
+
+rm -rf ${tmp_dir}
+
+echo "-- All done!"
 
