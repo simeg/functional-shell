e8243d831bf5020cd78d121b8fe7c228113d8136
Simon Egersand <s.egersand@gmail.com>
2020-02-07 19:54:01 +0100

    Exit with 1 if no input

--- tests/integration-tests/main.py
+++ tests/integration-tests/main.py
@@ -4,11 +4,6 @@ import sys
 import pytest
 import subprocess
 
-# def setup_module(module):
-    # Copy files to PATH
-
-# def teardown_module(module):
-    # Remove files from PATH
 
 def run(cmd):
     proc = subprocess.Popen(cmd,
@@ -23,6 +18,13 @@ def test_map__exits_w_code_1__when_no_function_provided():
     code, _, _ = run(["map"])
     assert code == 1
 
+def test_map__exits_w_code_1__when_no_input():
+    code, _, _ = run(["map", "add"])
+    assert code == 1
+
+    code, _, _ = run(["map", "add", "2"])
+    assert code == 1
+
 def test_map__writes_to_stderr__when_non_matching_function():
     code, stdout, stderr = run(["map", "non_matching_fn", "2>&1",
                                 ">/dev/null"])
@@ -32,6 +34,10 @@ def test_map__writes_to_stderr__when_non_matching_function():
 
 def test_filter__exits_w_code_1__when_no_function_provided():
     code, _, _ = run(["filter"])
+    assert code == 1
+
+def test_filter__exits_w_code_1__when_no_input():
+    code, _, _ = run(["filter", "is_file"])
     assert code == 1
 
 def test_filter__writes_to_stderr__when_non_matching_function():
